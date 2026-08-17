6d99d2e72c9087c0d29161d8bf5b05a7e1513e8c
etnstudio1 <etnhamid1@gmail.com>
2026-08-17 21:14:51 +0500

    Create SearchScreen.kt

---
+++ app/src/main/java/com/etnstudio/user/presentation/search/SearchScreen.kt
@@ -0,0 +1,10 @@
+package com.etnstudio.user.presentation.search
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun SearchScreen(navController: NavController) {
+    Text("Search Screen - Coming soon")
+}

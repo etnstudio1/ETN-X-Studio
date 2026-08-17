1b0842c6b6fb72bd4ceed20c2090e41ffc5f9000
etnstudio1 <etnhamid1@gmail.com>
2026-08-17 21:16:18 +0500

    Create PlayerScreen.kt

---
+++ app/src/main/java/com/etnstudio/user/presentation/player/PlayerScreen.kt
@@ -0,0 +1,9 @@
+package com.etnstudio.user.presentation.player
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PlayerScreen(itemId: String) {
+    Text("Player for: $itemId")
+}
